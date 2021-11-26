c16a92d5e6b375db92e5618718e8cbc3cc3b7ea9
Matt Welke <mattwelke@gmail.com>
2021-11-26 17:01:43 -0500

    Credit card numbers complete.

--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+)
+
+const (
+	creditCardRedactStub = "PII_CREDIT_CARD"
+)
+
+var creditCardRegex *regexp.Regexp
+
+func init() {
+	creditCardRegex = regexp.MustCompile(`\d{4}[ -]?\d{4}[ -]?\d{4}[ -]?\d{4}|\d{4}[ -]?\d{6}[ -]?\d{4}\d?`)
+}
+
+// redactResult is the result of a redaction process for any type of PII.
+// Includes data about the redaction that occurred which can be useful for
+// metrics.
+type redactResult struct {
+	// The number of times a substring in the source string was redacted.
+	numRedacted int64
+	// The string with all target substrings removed by the redaction process.
+	redacted []byte
+}
+
+// redactCreditCard redacts credit card numbers from strings.
+func redactCreditCard(src []byte) redactResult {
+	numRedacted := int64(0)
+
+	processed := creditCardRegex.ReplaceAllFunc(src, func(_ []byte) []byte {
+		numRedacted += 1
+		return []byte(creditCardRedactStub)
+	})
+
+	return redactResult{
+		numRedacted: numRedacted,
+		redacted:    processed,
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	creditCardNumber := "4242424242424242"
+	sourceString := fmt.Sprintf("My credit card number is %s. Please don't log it!", creditCardNumber)
+
+	res := redactCreditCard([]byte(sourceString))
+
+	if res.numRedacted == 0 {
+		fmt.Printf("Nothing to redact. Source string:\n%s\n", sourceString)
+	} else {
+		fmt.Printf("Redacted credit card number %d time(s). Processed string:\n%s\n", res.numRedacted, res.redacted)
+	}
 }
---
+++ main_test.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func Test_redactCreditCard(t *testing.T) {
+	// Fixture source: https://stripe.com/docs/testing
+	testCreditCardNumbers := [][]byte{
+		// Visa
+		[]byte("4242424242424242"),
+		[]byte("4000056655665556"),
+
+		// Mastercard
+		[]byte("5555555555554444"),
+		[]byte("2223003122003222"),
+		[]byte("5200828282828210"),
+		[]byte("5105105105105100"),
+
+		// American Express
+		[]byte("378282246310005"),
+		[]byte("371449635398431"),
+
+		// Discover
+		[]byte("6011111111111117"),
+		[]byte("6011000990139424"),
+
+		// Diners Club
+		[]byte("3056930009020004"),
+		[]byte("36227206271667"),
+
+		// JCB
+		[]byte("3566002020360505"),
+
+		// UnionPay
+		[]byte("6200000000000005"),
+	}
+
+	testCreditCardNumbersStylized := [][]byte{
+		// Visa
+		[]byte("4242 4242 4242 4242"),
+		[]byte("4000 0566 5566 5556"),
+
+		// Mastercard
+		[]byte("5555 5555 5555 4444"),
+		[]byte("2223 0031 2200 3222"),
+		[]byte("5200 8282 8282 8210"),
+		[]byte("5105 1051 0510 5100"),
+
+		// American Express
+		[]byte("3782 822463 10005"),
+		[]byte("3782 822463 10005"),
+
+		// Discover
+		[]byte("6011 1111 1111 1117"),
+		[]byte("6011 0009 9013 9424"),
+
+		// Diners Club
+		[]byte("3056 9300 0902 0004"),
+		[]byte("3622 720627 1667"),
+
+		// JCB
+		[]byte("3566 0020 2036 0505"),
+
+		// UnionPay
+		[]byte("6200 0000 0000 0005"),
+	}
+
+	allCreditCardNumbers := append(testCreditCardNumbers, testCreditCardNumbersStylized...)
+
+	for _, ccNum := range allCreditCardNumbers {
+		type args struct {
+			src []byte
+		}
+		tests := []struct {
+			name string
+			args args
+			want redactResult
+		}{
+
+			{
+				name: "Can sanitize a string that is a credit card number, reporting 1 redaction.",
+				args: args{ccNum},
+				want: redactResult{
+					numRedacted: 1,
+					redacted:    []byte(creditCardRedactStub),
+				},
+			},
+			{
+				name: "Can sanitize a string contains a credit card number, reporting 1 redaction.",
+				args: args{[]byte(fmt.Sprintf(" %s ", ccNum))},
+				want: redactResult{
+					numRedacted: 1,
+					redacted:    []byte(fmt.Sprintf(" %s ", creditCardRedactStub)),
+				},
+			},
+			{
+				name: "Can sanitize a string that contains multiple credit card numbers, reporting 2 redactions.",
+				args: args{[]byte(fmt.Sprintf("%s %s", ccNum, ccNum))},
+				want: redactResult{
+					numRedacted: 2,
+					redacted:    []byte(fmt.Sprintf("%s %s", creditCardRedactStub, creditCardRedactStub)),
+				},
+			},
+			{
+				name: "Can sanitize a string that contains multiple credit card numbers, separated by a newline, reporting 2 redactions.",
+				args: args{[]byte(fmt.Sprintf("%s\n%s", ccNum, ccNum))},
+				want: redactResult{
+					numRedacted: 2,
+					redacted:    []byte(fmt.Sprintf("%s\n%s", creditCardRedactStub, creditCardRedactStub)),
+				},
+			},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				if got := redactCreditCard(tt.args.src); !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("redactCreditCard() = %v, want %v", got, tt.want)
+				}
+			})
+		}
+	}
+}
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			main()
+		})
+	}
+}
